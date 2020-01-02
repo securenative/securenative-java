b43337212538aa5157535f5913ea5d64b27989d6
Inbal Tako <inbal@securenative.com>
2020-01-02 18:07:08 +0200

    Add interceptor manager

---
+++ sdk-base/src/main/java/com/securenative/snlogic/InterceptorManager.java
@@ -0,0 +1,11 @@
+package com.securenative.snlogic;
+
+public class InterceptorManager {
+    // TODO implement me
+    public InterceptorManager() {
+    }
+
+    public void applyInterceptors() {
+        
+    }
+}
