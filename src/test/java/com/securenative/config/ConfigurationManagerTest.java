d349831ab060434adbcbd4e3418e2eabc466ad35
Inbal Tako <inbal@securenative.com>
2020-07-26 15:50:05 +0300

    Disable windows unitest compitability

--- src/test/java/com/securenative/config/ConfigurationManagerTest.java
+++ src/test/java/com/securenative/config/ConfigurationManagerTest.java
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class NotRunningOnWindows implements IgnoreCondition {
+class NotRunningOnWindows implements IgnoreCondition {
     public boolean isSatisfied() {
         return !System.getProperty("os.name").startsWith("Windows");
     }
